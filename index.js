f635da952816f8dcdfff7a358876a0af57e31fc2
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-14 10:10:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solutions in this file
-
+const results = ['Lisa', 'Kaitlin', 'Jan'];
 function writeCards(name,event){
   for(let i=0; i < name.length; i++){
-  const results = ['Lisa', 'Kaitlin', 'Jan'];
+  
      results.push=( `Thank you, ${name[i]}, for the wonderful ${event} gift!`) ;
   }
   return results;
