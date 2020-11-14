05a6dbe963440d08c1511066e20cc7b77955f5b8
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-14 09:08:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name,event){
+  for(let i=0; i < name.length; i++){
+  
+   return [ `Thank you, ${name[i]}, for the wonderful ${event} gift!` ];
+  }
+}
+
+
+function countDown(q){
+  while (q>=0){
+    console.log(q--)
+  }
+}
